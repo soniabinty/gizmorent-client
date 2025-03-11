8e65d5afc343b8c330a925ced99acb25696bf9f1
Sonia Binty <sonianazninbinty@gmail.com>
2025-03-12 03:08:38 +0600

    added initial setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  
+} from "react-router-dom";
+import Root from "../MainLayout/Root";
+import Home from "../pages/Home/Home";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
