0b4a47e3a46e5383a8ca1337ce7f7b2b8a8cd42b
Shakir_Hasan <sakirhasan133@gmail.com>
2025-03-13 01:43:10 +0600

    TopContributor innitial design done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import TopRented from './TopRented';
+import TopRented from "./TopRented";
+import TopContributor from "./TopContributor";
 
 const Home = () => {
   return (
     <div>
-  <main className='mx-12'>
-     <TopRented></TopRented>
-  </main>
- 
+      <main className="mx-12">
+        <TopRented></TopRented>
+        <TopContributor></TopContributor>
+      </main>
     </div>
   );
 };
