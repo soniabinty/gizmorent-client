cb697e8822fb3b613ba0099aed84db1fddbf3cf2
abnahid <aznahid507@gmail.com>
2025-03-14 00:43:46 +0600

    feat: add footer component and real-time booking section; update layout and environment variables

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import CategorySlider from "./Categories";
-import TopRented from "./TopRented";
-import TopContributor from "./TopContributor";
 import AboutGizmoRent from "./AboutGizmoRent";
+import CategorySlider from "./Categories";
+import RealTimeBooking from "./RealTimeBooking";
+import TopContributor from "./TopContributor";
+import TopRented from "./TopRented";
 
 const Home = () => {
   return (
     <div>
-      <main className="md:mx-12">
+      <main>
         <CategorySlider></CategorySlider>
         <TopRented></TopRented>
         <TopContributor></TopContributor>
         <AboutGizmoRent></AboutGizmoRent>
+        <RealTimeBooking></RealTimeBooking>
       </main>
     </div>
   );
