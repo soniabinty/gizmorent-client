4cdad9ea49fea102747e04d62bf1f012b3bbad31
abnahid <aznahid507@gmail.com>
2025-04-06 22:49:42 +0600

    locked after three attempt like bkash pin locked

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import failedAttemptsReducer from './failedAttemptsSlice';
 import gadgetReducer from "./Feature/gadgetSlice";
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         gadgets: gadgetReducer,
+        failedAttempts: failedAttemptsReducer,
     },
 });
 
