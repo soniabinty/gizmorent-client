bb1ed1dce04565ee0bea84cafb609db7cb32f66f
abnahid <aznahid507@gmail.com>
2025-04-08 01:43:44 +0600

    Add SweetAlert and Axios integration for gadget management; implement update and delete functionalities

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -3,15 +3,16 @@ import authReducer from "./authSlice";
 import failedAttemptsReducer from './failedAttemptsSlice';
 import gadgetReducer from "./Feature/gadgetSlice";
 import reviewReducer from "./Feature/reviewSlice";
-import wishlistReducer from "./wishlistSlice"; 
+import wishlistReducer from "./wishlistSlice";
 const store = configureStore({
 
-  reducer: {
-    auth: authReducer,
-    gadgets: gadgetReducer,
-    reviews: reviewReducer,
-    wishlist: wishlistReducer,
-  },
+    reducer: {
+        auth: authReducer,
+        gadgets: gadgetReducer,
+        reviews: reviewReducer,
+        wishlist: wishlistReducer,
+        failedAttempts: failedAttemptsReducer,
+    },
 
 });
 
